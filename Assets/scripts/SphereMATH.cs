5a18fc515d2dc77a6c7ff13bcec7683fd151d10c
Михаил Кузнецов <kuznetsOFFmikhail@outlook.com>
2019-10-17 11:11:12 +0500

    маятник
    Добавлен маятник c c использованием joint и покоординатной отрисоки
    
    Co-Authored-By: justjune <justjune@users.noreply.github.com>

---
+++ Assets/scripts/SphereMATH.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMATH : MonoBehaviour {
+
+		Rigidbody rb;
+		public float A = 0.01F;
+		public float k = 0.05F;
+		float Omega;
+	Vector3 now;
+		void Start()
+		{
+			rb = GetComponent<Rigidbody>();
+			ls ();
+
+		Debug.Log (rb.position);
+		now = rb.position;
+		}
+
+		void ls(){
+			Omega = Mathf.Sqrt (k / rb.mass);
+		}
+
+		void Update()
+		{
+		rb.MovePosition( now + new Vector3( A*Mathf.Cos(Omega*Time.realtimeSinceStartup),				//x
+											-A*Mathf.Abs(Mathf.Sin(Omega*Time.realtimeSinceStartup)),	//y
+											0f) );														//z
+		}
+}
